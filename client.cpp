1d54905ab9b24f9fb50792825c343d0d76cf9d1c
Dylan <djykbd@mst.edu>
2015-04-19 15:25:23 -0500

    port number added

--- client.cpp
+++ client.cpp
@@ -7,6 +7,7 @@
 #include <netinet/in.h>  /* define internet socket */
 #include <netdb.h>       /* define internet socket */
 
+#define SERVER_PORT 9999     /* define a server port number */
 
 int main()
 {

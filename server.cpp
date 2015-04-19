5a2f3c0db8a5907f4b9b354375c6a8e293b413c5
Dylan <djykbd@mst.edu>
2015-04-19 14:21:23 -0500

    Added include files from example

---
+++ server.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>  /* define socket */
+#include <netinet/in.h>  /* define internet socket */
+#include <netdb.h>       /* define internet socket */
+
+
+
+
+int main()
+{
+
+  
+  
+  
+  return 0;
+}

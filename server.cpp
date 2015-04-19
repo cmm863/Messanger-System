3a31e94b2408f1fbb9aee9df9a673b4bd2028f73
Dylan <djykbd@mst.edu>
2015-04-19 15:24:32 -0500

    Added code from server1 ex with slight variatons for c++ and assignment specifics

--- server.cpp
+++ server.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+//#include <mutex>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,15 +7,68 @@
 #include <sys/socket.h>  /* define socket */
 #include <netinet/in.h>  /* define internet socket */
 #include <netdb.h>       /* define internet socket */
+using namespace std;
 
-
-
+#define SERVER_PORT 9999        /* define a server port number */
+#define MAX_CLIENT 10
 
 int main()
 {
+  int ns, sd, k;
+  int FDarray[MAX_CLIENT];   /* allocate file descriptors 
+                                for number of clients*/
+  sockaddr_in server_addr = { AF_INET, htons( SERVER_PORT ) };
+  sockaddr_in client_addr = { AF_INET }; 
+  unsigned int client_len = sizeof(client_addr);
+  int counter;
+  //mutex m;
+  char buf[512];
+  
+  //Create stream socket
+  if((sd=socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("server: socket failed");
+    exit(1);
+  }
+  
+  //bind socket to an internet port
+  if(bind(sd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+  {
+    perror("server: bind failed");
+    exit(1);
+  } 
+  
+  //listen for clients
+  if(listen(sd, 10) == -1)
+  {
+    perror("server: listen failed");
+    exit(1);
+  }
+  
+  cout << "SERVER is listening for clients to establish a connection " << endl;
+  
+  if((ns=accept(sd, (sockaddr*)&client_addr, &client_len)) == -1)
+  {
+    perror("server: accept failed");
+    exit(1);
+  }
+  
+  cout << "accept() successful.. a client has connected! waiting for a message" 
+       << endl;
+       
+  
+  while((k=read(ns, buf, sizeof(buf))) != 0)
+  {
+    printf("SERVER RECEIVED: %s\n", buf);
+    write(ns, buf, k);
+  }  
+  close(ns);
+  close(sd);
+  unlink((const char*)&server_addr.sin_addr);
+  
+  
+  
+  
 
-  
-  
-  
   return 0;
 }

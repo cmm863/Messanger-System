8452f2f48fef6924ddc1fc5a80f5d1f3545ec88e
Dylan <djykbd@mst.edu>
2015-04-22 11:36:48 -0500

    Added thread for read

--- client.cpp
+++ client.cpp
@@ -12,6 +12,12 @@ using namespace std;
 #define SERVER_PORT 8333     /* define a server port number */
 
 void sig_hand(int sig);
+void *reader(void *);
+struct clients
+{
+  int sd;
+  string name;
+};
 
 int main(int argc, char* argv[]) {
   int sd;
@@ -19,48 +25,57 @@ int main(int argc, char* argv[]) {
   string test;
   char buf[512];
   struct hostent *hp;
-  
+  string hostname, nickname;
+  pthread_t myclient;
   signal(SIGINT, sig_hand);
+  clients client; 
   
-  if(argc != 2) {
-    cout << "Usage: " << argv[0] << " hostname" << endl;
-    exit(1);
-  }
-
+  cout << "Enter hostname: ";
+  cin >> hostname;
+  
   // Get the host
-  if((hp = gethostbyname(argv[1])) == NULL) {
-    cout << argv[0] << ": " << argv[1] << " unknown host" << endl;
+  if((hp = gethostbyname(hostname.c_str())) == NULL) {
+    cout << argv[0] << ": " << hostname << " unknown host" << endl;
     exit(1);
   }
+  cout << "Enter username: ";
+  cin.ignore();
+  getline(cin,  client.name);
+  
 
   bcopy(hp->h_addr_list[0], (char*)&server_addr.sin_addr, hp->h_length);
 
   // Create a socket
-  if((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+  if((client.sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
     cout << "Client: Socket failed." << endl;
     exit(1);
   }
 
-  if(connect(sd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+  if(connect(client.sd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
     cout << "Client: Connect failed." << endl;
     exit(1);
   }
 
-  cout << "connect() successful! Will send a message to server." << endl;
-  cout << "Input a string: ";
-  //cin >> test;
-
+  for(int i=0; i<client.name.length()+1; i++)
+  {
+    buf[i]=client.name[i];
+  }
+  write(client.sd, buf, sizeof(buf));
+  read(client.sd, buf, sizeof(buf));
+  printf("%s\n", buf);  
+  
+  pthread_create(&myclient, NULL, reader, &client);
+  
   while(getline(cin, test)!=NULL&&test!="/exit"&&test!="/quit"&&test!="/part")
   {
     for(int i=0; i<test.length()+1; i++)
     {
       buf[i]=test[i];
     }
-    write(sd, buf , sizeof(buf));
-    read(sd, buf , sizeof(buf));
-    printf("SERVER ECHOED: %s\n", buf);
+    write(client.sd, buf , sizeof(buf));
   }
   
+  pthread_join(myclient, NULL)
   close(sd);
 
   return 0;
@@ -70,4 +85,21 @@ void sig_hand(int sig)
 {
   cout << "\nIf you wish to exit, please type /exit /quit/ or /part" << endl;
   return;
+}
+
+void *reader(void *passed_client)
+{
+  clients *client = (clients *)passed_client;
+  char buf[512];
+  
+  while(read(client->sd, buf , sizeof(buf)) > 0)
+  {
+    if(buf==client->name);
+    else
+    {
+      read(client->sd, buf , sizeof(buf));
+      printf("%s\n", buf);
+    }
+  }
+  pthread_exit(passed_client);
 }

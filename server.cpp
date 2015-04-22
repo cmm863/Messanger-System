07d2f37c7e161dc92a464184c0e272a81ec4878a
Connor McBride <cmm863@mst.edu>
2015-04-22 11:26:32 -0500

    Added threading support to the new server.cpp. The SERVER_PORT is all messed up...

--- server.cpp
+++ server.cpp
@@ -7,11 +7,20 @@
 #include <sys/socket.h>  /* define socket */
 #include <netinet/in.h>  /* define internet socket */
 #include <netdb.h>       /* define internet socket */
+#include <pthread.h>
 using namespace std;
 
-#define SERVER_PORT 8333        /* define a server port number */
+#define SERVER_PORT 8443        /* define a server port number */
 #define MAX_CLIENT 10
 
+void *task(void *arguments);
+
+struct arg_struct {
+  int ns;
+};
+
+static int connFd;
+static int test_array[MAX_CLIENT];
 int main()
 {
   int ns, sd, k;
@@ -21,10 +30,12 @@ int main()
   sockaddr_in client_addr = { AF_INET }; 
   unsigned int client_len = sizeof(client_addr);
   int counter;
-  string test;
+
+  pthread_t threadA[3];
   
-  //mutex m;
-  char buf[512];
+  for(int i = 0; i < MAX_CLIENT; i++) {
+    test_array[i] = -1;
+  }
   
   //Create stream socket
   if((sd=socket(AF_INET, SOCK_STREAM, 0)) == -1)
@@ -46,27 +57,55 @@ int main()
     perror("server: listen failed");
     exit(1);
   }
-  
+  int noThread = 0;
   cout << "SERVER is listening for clients to establish a connection " << endl;
-  
-  if((ns=accept(sd, (sockaddr*)&client_addr, &client_len)) == -1)
-  {
-    perror("server: accept failed");
-    exit(1);
+
+  struct arg_struct args;
+  while(noThread < MAX_CLIENT) {
+    cout << "Listening" << endl;
+    struct arg_struct args;
+    connFd = accept(sd, (sockaddr*)&client_addr, &client_len);
+    args.ns = connFd;
+    test_array[noThread] = connFd;
+
+    if(connFd == -1) {
+      cerr << "Cannot accept connection." << endl;
+      exit(1);
+    }
+    else {
+      cout << "Connection successful." << endl;
+    }
+    if(pthread_create(&threadA[noThread], NULL, task, (void *)&args) != 0) {
+      cout << "Thread create failed." << endl;
+      exit(1);
+    }
+    noThread++;
   }
   
   cout << "accept() successful.. a client has connected! waiting for a message" 
        << endl;
        
-  
-  while((k=read(ns,  buf, sizeof(buf))) != 0)
-  {
-    printf("SERVER RECEIVED: %s\n", buf);
-    write(ns, buf , k);
-  }  
-  close(ns);
   close(sd);
   unlink((const char*)&server_addr.sin_addr);
   
   return 0;
+}
+
+void *task(void *arguments) {
+  struct arg_struct *args = (struct arg_struct *)arguments;
+  int k;
+  char buf[512];
+  while((k=read(args->ns, buf, sizeof(buf))) != 0) {
+    cout << args->ns << endl;
+    printf("SERVER RECEIVED: %s\n", buf);
+    for(int i = 0; i < MAX_CLIENT; i++) {
+      if(test_array[i] != -1) {
+        write(test_array[i], buf, sizeof(buf));
+        cout << "Sent to: " << test_array[i] << endl;
+      }
+    }
+  }
+  close(args->ns);
+
+  return NULL;
 }
